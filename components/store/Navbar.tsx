b57b01df8bc76c1bf969f4bef88c5112c5a7bb8e
Temesgen123 <birechis@gmail.com>
2026-06-27 11:15:33 +0300

    Changed Logo Display Mode for Mobile.

--- components/store/Navbar.tsx
+++ components/store/Navbar.tsx
@@ -55,7 +55,7 @@ export default function Navbar({
               alt="Logo"
               width={50}
               height={50}
-              className="rounded-full"
+              className="rounded-full hidden sm:block"
             />
             My<span style={{ color: 'var(--accent)' }}>Store</span>
           </Link>
