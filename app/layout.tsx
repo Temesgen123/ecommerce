199ee702336a56bc69fc4840b00a9e4dac06e1a6
Temesgen123 <birechis@gmail.com>
2026-07-27 10:48:26 +0300

    Edited root layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
 import type { Metadata } from 'next';
-//import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/next';
 
-// const geistSans = Geist({
-//   variable: '--font-geist-sans',
-//   subsets: ['latin'],
-//   display: 'swap',
-//   preload: false, // ← prevents fetching at build time
-// });
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+  display: 'swap',
+  preload: false, // ← prevents fetching at build time
+});
 
-// const geistMono = Geist_Mono({
-//   variable: '--font-geist-mono',
-//   subsets: ['latin'],
-//   display: 'swap',
-//   preload: false, // ← prevents fetching at build time
-// });
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+  display: 'swap',
+  preload: false, // ← prevents fetching at build time
+});
 
 const baseUrl = process.env.NEXTAUTH_URL ?? 'http://localhost:3000';
 
@@ -82,8 +82,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      className="antialiased"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
       data-scroll-behavior="smooth"
     >
